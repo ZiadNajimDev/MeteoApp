ee2f130d12a9da20858b6c2a08c4ec958743b595
ZiadNajimDev <ziad.najim@hotmail.com>
2025-04-14 00:14:57 +0100

    Add initial project files

---
+++ meteoApp/meteoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  meteoAppApp.swift
+//  meteoApp
+//
+//  Created by user277447 on 4/13/25.
+//
+
+import SwiftUI
+
+@main
+struct meteoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ meteoAppTests/meteoAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  meteoAppTests.swift
+//  meteoAppTests
+//
+//  Created by user277447 on 4/13/25.
+//
+
+import Testing
+@testable import meteoApp
+
+struct meteoAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
